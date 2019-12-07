e825d60133fff4dd662a32249e098258cfdb2b4b
Alberto <albertoyanesjr3@hotmail.com>
2019-12-07 11:33:46 -0300

    [FEAT] Added initial setup for Storybook

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+const Button = props => (
+  <button onClick={props.onClick} style={props.style}>
+    {props.label && <span>{props.label}</span>}
+  </button>
+);
+export default Button;
